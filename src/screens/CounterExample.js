069686abd19e218f270b48936ca527fc66f71eed
Vogel <hendrik.vogel@cubic.com>
2021-01-23 17:53:17 +0100

    added eslint code quality tool

--- src/screens/CounterExample.js
+++ src/screens/CounterExample.js
@@ -1,22 +1,24 @@
-import React, {useState} from 'react';
-import { View, Button, Text, StyleSheet } from "react-native";
- 
-const styles=StyleSheet.create({
-    counter: {
-        textAlign: 'center'
-    }
+import React, { useState } from 'react';
+import {
+  View, Button, Text, StyleSheet,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  counter: {
+    textAlign: 'center',
+  },
 });
 
 const CounterExample = () => {
-    const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-    return (
-        <View style={StyleSheet}>
-            <Button onPress={() => setCounter(counter - 1)} title="Decrease" /> 
-            <Text style={styles.counter}>{counter}</Text>
-            <Button onPress={() => setCounter(counter + 1)} title="Increase" />
-        </View>
-    )
+  return (
+    <View style={StyleSheet}>
+      <Button onPress={() => setCounter(counter - 1)} title="Decrease" />
+      <Text style={styles.counter}>{counter}</Text>
+      <Button onPress={() => setCounter(counter + 1)} title="Increase" />
+    </View>
+  );
 };
 
 export default CounterExample;
--- src/screens/DummyScreen.js
+++ src/screens/DummyScreen.js
@@ -1,26 +1,29 @@
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
+import {
+  SafeAreaView, Text, StyleSheet, Button,
+} from 'react-native';
 import * as React from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { Button } from 'react-native';
 
-const styles = StyleSheet.create({ 
-    container: {
-        flex: 1,
-        alignContent: 'center',
-        justifyContent: 'center'
-    },
-    helloWorld: {
-        textAlign: 'center'
-    }
- });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  helloWorld: {
+    textAlign: 'center',
+  },
+});
 
 const DummyScreen = () => {
-    const { navigate } = useNavigation();
+  const { navigate } = useNavigation();
 
-    return (<SafeAreaView style={styles.container}>
-        <Text style={styles.helloWorld}>Hello world</Text>
-        <Button onPress={() => navigate("Dummy2")} title="Navigate" />
-    </SafeAreaView>)
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.helloWorld}>Hello world</Text>
+      <Button onPress={() => navigate('Dummy2')} title="Navigate" />
+    </SafeAreaView>
+  );
 };
 
 export default DummyScreen;
