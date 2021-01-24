00afdf805ed1294acabd6ef5c7fcb843fcb2d803
Vogel <hendrik.vogel@cubic.com>
2021-01-24 20:04:34 +0100

    WIP: visual enhancements

--- src/components/Cup.js
+++ src/components/Cup.js
@@ -1,27 +1,80 @@
-import React from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import {
+  TouchableWithoutFeedback, View, Image, Animated,
+} from 'react-native';
+
+const cupImage = require('../../assets/images/Cup.png');
 
 const Cup = ({ cupSize, onPress, data }) => {
+  const translateY = useRef(new Animated.Value(300)).current;
+  const scale = useRef(new Animated.Value(1)).current;
+  const [ballVisible, setBallVisible] = useState(false);
+
+  const animateClose = () => {
+    setBallVisible(true);
+    Animated.parallel([
+      Animated.timing(translateY, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: true,
+      }),
+      Animated.sequence([
+        Animated.timing(scale, {
+          toValue: 2,
+          duration: 250,
+          useNativeDriver: true,
+        }),
+        Animated.timing(scale, {
+          toValue: 1,
+          duration: 250,
+          useNativeDriver: true,
+        }),
+      ]),
+    ]).start(({ finished }) => {
+      if (finished) {
+        setTimeout(() => { onPress(data); }, 200);
+      }
+    });
+  };
+
   if (!data.active) {
     return <View style={{ height: cupSize, width: cupSize }} />;
   }
 
   return (
-    <TouchableOpacity
+    <TouchableWithoutFeedback
       style={{
         width: cupSize,
         height: cupSize,
         borderRadius: cupSize / 2,
-        backgroundColor: '#fff',
-        borderColor: 'red',
-        borderWidth: 2,
         alignContent: 'center',
         justifyContent: 'center',
       }}
-      onPress={() => onPress(data)}
+      onPress={animateClose}
     >
-      <View style={{}} />
-    </TouchableOpacity>
+      <View>
+        <Image style={{ height: cupSize, width: cupSize }} source={cupImage} />
+        {ballVisible && (
+        <Animated.View style={[{
+          height: cupSize / 2,
+          width: cupSize / 2,
+          backgroundColor: '#fff',
+          borderWidth: 1,
+          borderColor: 'grey',
+          position: 'absolute',
+          borderRadius: cupSize / 4,
+          left: cupSize / 4,
+          zIndex: 99,
+        }, {
+          transform: [
+            { translateY },
+            { scale },
+          ],
+        }]}
+        />
+        )}
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
--- src/components/TableContainer.js
+++ src/components/TableContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import {
   View, StyleSheet, Text, ScrollView,
 } from 'react-native';
@@ -7,26 +7,22 @@ import CupContainer from './CupContainer';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margin: 30,
-
+    margin: 15,
+    padding: 15,
+    borderRadius: 15,
+    borderWidth: 5,
+    borderColor: '#616163',
+    borderBottomWidth: 0,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    backgroundColor: '#e71d36',
   },
 });
 
-const TableContainer = () => {
-  const [log, setLog] = useState('---MATCH START----\n');
-
-  const logEvent = (event) => {
-    setLog(`${log}\n${JSON.stringify(event, null, 2)}`);
-  };
-
-  return (
-    <View style={styles.container}>
-      <CupContainer logEvent={logEvent} />
-      <ScrollView style={{ flex: 1 }}>
-        <Text>{log}</Text>
-      </ScrollView>
-    </View>
-  );
-};
+const TableContainer = ({ logEvent }) => (
+  <View style={styles.container}>
+    <CupContainer logEvent={logEvent} />
+  </View>
+);
 
 export default TableContainer;
