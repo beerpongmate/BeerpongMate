dc5fa395e11b7cff27e17a87a590d0102e896195
Hendrik Vogel <hendrik.vgl@gmail.com>
2021-04-14 20:54:38 +0200

    added backend stats integration

--- src/components/Providers/useStats.js
+++ src/components/Providers/useStats.js
@@ -2,63 +2,139 @@ import firestore from "@react-native-firebase/firestore";
 import { useState, useEffect } from "react";
 
 const initialStats = {
-    matchCount: 0,
-    matchWins: 0,
-    matchLosses: 0,
-    throwCount: 0,
-    hitCount: 0
+  matchCount: 0,
+  matchWins: 0,
+  matchLosses: 0,
+  throwCount: 0,
+  hitCount: 0,
+  bestStreak: 0,
+  streak: 0,
+  bestHitStreak: 0
+};
+
+const getLongestPlayerHitStreak = (throws) => {
+  const normalizedArray = throws.map(({ type }) => type === "HIT");
+
+  let longestStreak = 0;
+  let currentStreak = 0;
+
+  normalizedArray.forEach((hit) => {
+    if (hit) {
+      currentStreak += 1;
+    } else {
+      longestStreak = Math.max(currentStreak, longestStreak);
+      currentStreak = 0;
+    }
+  });
+  return Math.max(longestStreak, currentStreak);
 };
 
 const useStats = (userId, matchId) => {
-    const [stats, setStats] = useState(undefined);
-    const statsRef = firestore()
-            .collection('Stats')
-            .doc(userId);
+  const [stats, setStats] = useState(undefined);
+  const [recentMatches, setRecentMatches] = useState([]);
+  const statsRef = firestore().collection("Stats").doc(userId);
 
-    const fetchStats = () => {
-        statsRef.get().then(
-                ref => {
-                    if (ref.exists) {
-                        setStats(ref.data());
-                    } else {
-                        statsRef.set(
-                            initialStats
-                        ).then(fetchStats).catch(console.log);
-                    }
-                 }).catch(console.log);
-    };
-
-    const processMatch = async (matchData) => {
-        const matchStatRef = statsRef.collection('MatchStats').doc(matchId);
-
-        if (!matchStatRef.exists) {
-            const playerTeam = matchData?.players.find(({ uid }) => uid === userId)?.team;
-            const win = matchData?.winningTeam === playerTeam;
-            const teamThrows = matchData?.data?.throws[playerTeam]
-            const playerThrows = teamThrows.filter(({ playerId }) => playerId === userId);
-            const playerHits = playerThrows.filter(({ type }) => type === 'HIT');
-            const winInc = win ? 1 : 0;
-            const lossInc = win ? 0 : 1;
-        
-            const { hitCount, matchCount, matchLosses, matchWins, throwCount } = stats;
-            await statsRef.update({
-                hitCount: hitCount + playerHits.length,
-                matchCount: matchCount + 1,
-                matchLosses: matchLosses + lossInc,
-                matchWins: matchWins + winInc,
-                throwCount: throwCount + playerThrows.length
-            });
-            await matchStatRef.set({ teamThrows, win });
+  const fetchStats = () => {
+    statsRef
+      .get()
+      .then((ref) => {
+        if (ref.exists) {
+          setStats(ref.data());
+        } else {
+          statsRef.set(initialStats).then(fetchStats).catch(console.log);
         }
-    };
+      })
+      .catch(console.log);
+  };
 
-    useEffect(() => { 
-        fetchStats();
-    }, []);
+  const fetchRecentMatches = (limit = 5) => {
+    if (statsRef !== undefined) {
+      const matchArray = [];
+      statsRef
+        .collection("MatchStats")
+        .orderBy("timestamp", "desc")
+        .limit(limit)
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((documentSnapshot) => {
+            matchArray.push(documentSnapshot.data());
+          });
+          setRecentMatches(matchArray);
+        })
+        .catch(console.log);
+    }
+  };
 
-    
+  const processMatch = async (matchData) => {
+    const matchStatRef = statsRef.collection("MatchStats").doc(matchId);
+    if (!matchStatRef.exists) {
+      const playerTeam = matchData?.players.find(({ uid }) => uid === userId)
+        ?.team;
+      const win = matchData?.winningTeam === playerTeam;
+      const teamThrows = matchData?.data?.throws[playerTeam];
+      const playerThrows = teamThrows.filter(
+        ({ playerId }) => playerId === userId
+      );
+      const playerHits = playerThrows.filter(({ type }) => type === "HIT");
+      const winInc = win ? 1 : 0;
+      const lossInc = win ? 0 : 1;
 
-  return { stats, fetchStats, processMatch };
+      const team0Throws = matchData?.data?.throws[0];
+      const team0LastThrow = matchData?.data?.throws[0][team0Throws.length - 1];
+      const team0RemainingCups = Object.values(team0LastThrow.state).filter(
+        ({ active }) => active
+      ).length;
+
+      const team1Throws = matchData?.data?.throws[1];
+      const team1LastThrow = matchData?.data?.throws[1][team1Throws.length - 1];
+      const team1RemainingCups = Object.values(team1LastThrow.state).filter(
+        ({ active }) => active
+      ).length;
+
+      const longestHitStreak = getLongestPlayerHitStreak(playerThrows);
+
+      const {
+        hitCount,
+        matchCount,
+        matchLosses,
+        matchWins,
+        throwCount,
+        streak,
+        bestStreak,
+        bestHitStreak
+      } = stats;
+
+      const newStreak = win ? streak + 1 : 0;
+
+      await statsRef.update({
+        hitCount: hitCount + playerHits.length,
+        matchCount: matchCount + 1,
+        matchLosses: matchLosses + lossInc,
+        matchWins: matchWins + winInc,
+        throwCount: throwCount + playerThrows.length,
+        streak: newStreak,
+        bestStreak: Math.max(bestStreak, newStreak),
+        bestHitStreak: Math.max(bestHitStreak, longestHitStreak)
+      });
+      await matchStatRef.set({
+        playerThrows,
+        win,
+        playerTeam,
+        remainingCups: {
+          0: team0RemainingCups,
+          1: team1RemainingCups
+        },
+        hitRate: playerHits.length / playerThrows.length,
+        timestamp: firestore.FieldValue.serverTimestamp()
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchStats();
+  }, []);
+
+  return { stats, fetchStats, processMatch, recentMatches, fetchRecentMatches };
 };
 
 export default useStats;
--- src/screens/StatsScreen.js
+++ src/screens/StatsScreen.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import useStats from '../components/Providers/useStats';
+import React, { useEffect } from "react";
+
+import { View, StyleSheet } from "react-native";
+import useStats from "../components/Providers/useStats";
 import { useUser } from "../components/Providers/WithUser";
-import ThemedText from '../components/ThemedComponents/ThemedText';
+import StatItem from "../components/StatItem";
+import ThemedText from "../components/ThemedComponents/ThemedText";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 15
+  }
 });
 
 const StatsScreen = () => {
-      const { user } = useUser();
-    const { stats, fetchStats } = useStats(user.uid);
+  const { user } = useUser();
+  const { stats, fetchStats, recentMatches, fetchRecentMatches } = useStats(
+    user.uid
+  );
 
-    return (
-      <View style={styles.container}>
-        <ThemedText>{JSON.stringify(stats, null, 2)}</ThemedText>
-      </View>
-);
+  useEffect(() => {
+    fetchRecentMatches();
+  }, []);
+
+  const {
+    bestStreak,
+    bestHitStreak,
+    matchCount,
+    hitCount,
+    matchLosses,
+    throwCount,
+    matchWins,
+    streak
+  } = stats;
+
+  return (
+    <View style={styles.container}>
+      <StatItem stat={matchCount} label="Matches Played" />
+      <ThemedText>{JSON.stringify(stats, null, 2)}</ThemedText>
+      <ThemedText>{JSON.stringify(recentMatches, null, 2)}</ThemedText>
+    </View>
+  );
 };
 
 export default StatsScreen;
