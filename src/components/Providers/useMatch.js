63aa85115340c2af3f0640529b683c7c5ea8b109
Hendrik Vogel <hendrik.vgl@gmail.com>
2021-04-19 20:06:26 +0200

    added onFire and rebuttal

--- src/components/Providers/useMatch.js
+++ src/components/Providers/useMatch.js
@@ -1,5 +1,6 @@
 import firestore from "@react-native-firebase/firestore";
 import React, { useRef, useState } from "react";
+import takeRight from "lodash/takeRight";
 import { useFocusEffect } from "@react-navigation/native";
 
 const useMatch = (matchId, user) => {
@@ -10,21 +11,32 @@ const useMatch = (matchId, user) => {
 
   const { winningTeam } = match || {};
 
-  const setWinningTeam = (team) => matchRef.current.update({
+  const setWinningTeam = (team) =>
+    matchRef.current.update({
       winningTeam: team
     });
 
   if (winningTeam === undefined) {
+    const { pendingWinner } = match?.data || {};
+    const { team, rebuttalPlayers } = pendingWinner || {};
     const team0Throws = match?.data?.throws[0] || [];
-    const team0WinIndex = team0Throws.findIndex(({ hitId }) => hitId === '1-1');
-    const team1Throws = match?.data?.throws[1] || [];
-    const team1WinIndex = team1Throws.findIndex(({ hitId }) => hitId === '1-1');
+    console.log("TEAM0Throws:", team0Throws);
+    console.log("TEAM0ThrowCount:", team0Throws.length);
 
-    if (team1WinIndex > 0 && team1WinIndex > 0 && team0WinIndex === team1WinIndex) {
+    const team0WinIndex = team0Throws.findIndex(({ hitId }) => hitId === "1-1");
+    console.log("TEAM0WIN:", team0WinIndex);
+    const team1Throws = match?.data?.throws[1] || [];
+    console.log("TEAM1Throws:", team1Throws);
+    console.log("TEAM1ThrowCount:", team1Throws.length);
+
+    const team1WinIndex = team1Throws.findIndex(({ hitId }) => hitId === "1-1");
+    console.log("TEAM1WIN:", team1WinIndex);
+
+    if (team1WinIndex > 0 && team0WinIndex > 0) {
       setWinningTeam(-1);
-    } else if (team1WinIndex > 0 && team1WinIndex <= team0Throws.length - 1) {
+    } else if (team === 1 && rebuttalPlayers.length === 0) {
       setWinningTeam(1);
-    } else if (team0WinIndex > 0 && team0WinIndex <= team1Throws.length-1) {
+    } else if (team === 0 && rebuttalPlayers.length === 0) {
       setWinningTeam(0);
     }
   }
@@ -49,7 +61,25 @@ const useMatch = (matchId, user) => {
     }, [matchId])
   );
 
-  const getNextPlayerId = (playerId, order) => {
+  const getNextPlayerId = (playerId, order, teamThrows, rebuttalPlayers) => {
+    if (rebuttalPlayers && rebuttalPlayers.length > 0) {
+      return rebuttalPlayers[0];
+    }
+
+    // ON FIRE logic
+    const playerThrows = teamThrows.filter(
+      ({ playerId: id }) => playerId === id
+    );
+
+    const lastThreeThrows = takeRight(playerThrows, 3);
+
+    const filterHits = lastThreeThrows.filter(({ type }) => type === "HIT");
+
+    if (filterHits.length === 3) {
+      return playerId;
+    }
+    // END ON FIRE logic
+
     const currentIndex = order.findIndex((id) => id === playerId);
     if (order.length - 1 === currentIndex) {
       return order[0];
@@ -58,18 +88,57 @@ const useMatch = (matchId, user) => {
   };
 
   const addThrow = (data) => {
-    const { data: matchData, players} = matchDataRef.current || {};
-    const { order, throws } = matchData || {};
-    const player = players.find(({ uid }) => uid === user.uid)
-    const { team } = player || {};
-    throws[team] = [...(throws[team] || []) , data];
-    return matchRef.current.update({
-      data: {
-        throws,
-        playerTurn: getNextPlayerId(user.uid, order),
-        order,
-      },
-    });
+    if (winningTeam === undefined) {
+      const { data: matchData, players } = matchDataRef.current || {};
+      const { order, throws, pendingWinner } = matchData || {};
+      const player = players.find(({ uid }) => uid === user.uid);
+      const { team } = player || {};
+      throws[team] = [...(throws[team] || []), data];
+      const opponentTeam = team === 0 ? 1 : 0;
+      const opponentPlayers = players
+        .filter(({ team }) => team === opponentTeam)
+        .map(({ uid }) => uid);
+
+      const isWinningThrow = data?.hitId === "1-1";
+
+      let pendingEntry;
+
+      if (isWinningThrow) {
+        pendingEntry = {
+          pendingWinner: {
+            team,
+            rebuttalPlayers: opponentPlayers
+          }
+        };
+      } else if (pendingWinner?.rebuttalPlayers?.length > 0) {
+        if (data?.type === "HIT") {
+          pendingEntry = { pendingWinner };
+        } else {
+          pendingEntry = {
+            pendingWinner: {
+              ...pendingWinner,
+              rebuttalPlayers: pendingWinner?.rebuttalPlayers.filter(
+                (uid) => uid !== user.uid
+              )
+            }
+          };
+        }
+      }
+
+      return matchRef.current.update({
+        data: {
+          throws,
+          playerTurn: getNextPlayerId(
+            user.uid,
+            order,
+            throws[team],
+            pendingEntry?.pendingWinner?.rebuttalPlayers
+          ),
+          order,
+          ...pendingEntry
+        }
+      });
+    }
   };
 
   const createMatch = (data) => firestore().collection("Matches").add(data);
