1be5d2c6145c690faa77263d9320e7ed5969dcf0
maikmrfldt <maik.kummerfeldt@googlemail.com>
2021-04-21 23:28:49 +0200

    tried to implement async in mainstack, commented out for now

--- src/navigation/MainStack.js
+++ src/navigation/MainStack.js
@@ -18,7 +18,8 @@ import theme from "../../assets/theme";
 import OnboardingScreen from "../screens/OnboardingScreen";
 
 const Stack = createStackNavigator();
-  {/*const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+{/* const MainStack = () => {
+  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
 
 useEffect(() => {
   AsyncStorage.getItem("alreadyLaunched".then(value => {
@@ -28,18 +29,142 @@ useEffect(() => {
     } else {
       setIsFirstLaunch(false);
     }
-  }),
-},
-[]);
+  });
+},[]);
 
 if ( isFirstLaunch === null) {
   return null;
 } else if (isFirstLaunch === true) {
   return (
+<Stack.Navigator
+    initialRouteName="Onboarding"
+    options={{ headerShown: false }}
+    screenOptions={{
+      headerTitle: () => <View style={{ height: 20 }} />,
+      // headerTransparent: true,
+      headerStyle: {},
+      headerBackTitle: "Back",
+      headerTintColor: theme.colors.cupBlue,
+      headerTitleStyle: {}
+    }}
+  >
+    <Stack.Screen
+      name="Onboarding"
+      component={OnboardingScreen}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="MainDrawer"
+      component={MainDrawer}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="Welcome"
+      component={WelcomeScreen}
+      options={{ headerShown: false }}
+    />
 
-  )
-}
-*/}
+    <Stack.Screen name="SignIn" component={SignInScreen} />
+    <Stack.Screen
+      name="SignInSuccessScreen"
+      component={SignInSuccessScreen}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen name="SignUp" component={SignUpScreen} />
+    <Stack.Screen name="Online" component={OnlineScreen} />
+    <Stack.Screen
+      name="Lobby"
+      component={LobbyScreen}
+      options={{ headerBackTitle: "Leave" }}
+    />
+    <Stack.Screen name="Match" component={MatchScreen} />
+    <Stack.Screen
+      name="MatchLanding"
+      component={MatchLandingScreen}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="Stats"
+      options={{ headerTitle: "Lifetime Statistics" }}
+      component={StatsScreen}
+    />
+    <Stack.Screen name="Achievements" component={AchievementsScreen} />
+    <Stack.Screen
+      name="MainTab"
+      component={MainTabScreen}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen name="Rules" component={Rules} />
+  </Stack.Navigator>
+);
+  );
+} else {
+  <Stack.Navigator
+    initialRouteName="MainDrawer"
+    options={{ headerShown: false }}
+    screenOptions={{
+      headerTitle: () => <View style={{ height: 20 }} />,
+      // headerTransparent: true,
+      headerStyle: {},
+      headerBackTitle: "Back",
+      headerTintColor: theme.colors.cupBlue,
+      headerTitleStyle: {}
+    }}
+  >
+    <Stack.Screen
+      name="Onboarding"
+      component={OnboardingScreen}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="MainDrawer"
+      component={MainDrawer}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="Welcome"
+      component={WelcomeScreen}
+      options={{ headerShown: false }}
+    />
+
+    <Stack.Screen name="SignIn" component={SignInScreen} />
+    <Stack.Screen
+      name="SignInSuccessScreen"
+      component={SignInSuccessScreen}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen name="SignUp" component={SignUpScreen} />
+    <Stack.Screen name="Online" component={OnlineScreen} />
+    <Stack.Screen
+      name="Lobby"
+      component={LobbyScreen}
+      options={{ headerBackTitle: "Leave" }}
+    />
+    <Stack.Screen name="Match" component={MatchScreen} />
+    <Stack.Screen
+      name="MatchLanding"
+      component={MatchLandingScreen}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="Stats"
+      options={{ headerTitle: "Lifetime Statistics" }}
+      component={StatsScreen}
+    />
+    <Stack.Screen name="Achievements" component={AchievementsScreen} />
+    <Stack.Screen
+      name="MainTab"
+      component={MainTabScreen}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen name="Rules" component={Rules} />
+  </Stack.Navigator>
+);
+
+};
+
+export default MainStack;*/}
+
 const MainStack = () => (
   <Stack.Navigator
     initialRouteName="Onboarding"
